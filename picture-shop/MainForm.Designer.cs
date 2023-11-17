c5fd8a56de36268037d6684a722701a705eb1a75
Tom Gregor <tgregor@ivsoftware.com>
2023-11-17 06:44:22 -0700

    Add project files.

---
+++ picture-shop/MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace picture_shop
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            buttonDraw = new Button();
+            trackBarTolerance = new TrackBar();
+            labelTolerance = new Label();
+            buttonSave = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarTolerance).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(10, 10);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(458, 374);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // buttonDraw
+            // 
+            buttonDraw.Location = new Point(317, 405);
+            buttonDraw.Name = "buttonDraw";
+            buttonDraw.Size = new Size(132, 38);
+            buttonDraw.TabIndex = 1;
+            buttonDraw.Text = "Draw";
+            buttonDraw.UseVisualStyleBackColor = true;
+            // 
+            // trackBarTolerance
+            // 
+            trackBarTolerance.Location = new Point(13, 409);
+            trackBarTolerance.Maximum = 100;
+            trackBarTolerance.Name = "trackBarTolerance";
+            trackBarTolerance.Size = new Size(289, 69);
+            trackBarTolerance.TabIndex = 2;
+            trackBarTolerance.Value = 10;
+            trackBarTolerance.Visible = false;
+            // 
+            // labelTolerance
+            // 
+            labelTolerance.AutoSize = true;
+            labelTolerance.Location = new Point(101, 449);
+            labelTolerance.Name = "labelTolerance";
+            labelTolerance.Size = new Size(85, 25);
+            labelTolerance.TabIndex = 3;
+            labelTolerance.Text = "Tolerance";
+            labelTolerance.Visible = false;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(317, 444);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(132, 38);
+            buttonSave.TabIndex = 1;
+            buttonSave.Text = "Save";
+            buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(478, 484);
+            Controls.Add(labelTolerance);
+            Controls.Add(buttonSave);
+            Controls.Add(buttonDraw);
+            Controls.Add(trackBarTolerance);
+            Controls.Add(pictureBox1);
+            Name = "MainForm";
+            Padding = new Padding(10, 10, 10, 100);
+            Text = "PictureShop";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarTolerance).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button buttonDraw;
+        private TrackBar trackBarTolerance;
+        private Label labelTolerance;
+        private Button buttonSave;
+    }
+}
---
+++ picture-shop/MainForm.cs
@@ -0,0 +1,94 @@
+
+using System.Reflection;
+
+namespace picture_shop
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent(); 
+            ImageFolder =
+            Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Images"
+            );
+            buttonDraw.Click += onButtonDrawClick;
+            buttonSave.Click += (sender, e) => pictureBox1.Image?.Save(AppDataFolder);
+            pictureBox1.Paint += onPicturebox1Paint;
+            trackBarTolerance.ValueChanged += (sender, e) => pictureBox1.Refresh();
+        }
+
+        private int _clickCount = 0;
+        string ImageFolder { get; } = Path.Combine( 
+            AppDomain.CurrentDomain.BaseDirectory, 
+            "Images");
+        string AppDataFolder { get; } = Path.Combine( 
+            Environment.GetFolderPath( 
+                Environment.SpecialFolder.LocalApplicationData),
+                Assembly.GetExecutingAssembly().GetName().Name,
+                "Images");
+
+        /// <summary>
+        /// Add layer, then refresh PictureBox to draw.
+        /// </summary>
+        private void onButtonDrawClick(object? sender, EventArgs e)
+        {
+            switch ( _clickCount++ ) 
+            {
+                case 0:
+                    Layers.Add((Bitmap)Bitmap.FromFile(Path.Combine(ImageFolder, "Image1.png")));
+                    break;
+                case 1:
+                    Layers.Add((Bitmap)Bitmap.FromFile(Path.Combine(ImageFolder, "Image2.png")));
+                    labelTolerance.Visible = trackBarTolerance.Visible = true;
+                    break;
+            }
+            pictureBox1.Refresh();
+        }
+
+        private
+        List<Bitmap> Layers { get; } = new List<Bitmap>();
+        private void onPicturebox1Paint(object? sender, PaintEventArgs e)
+        {
+            Bitmap bmp;
+            for (int i = 0; i < Layers.Count; i++) 
+            {
+                switch (i)
+                {
+                    case 0:
+                        bmp = Layers[i];
+                        break;
+                    case 1:
+                        bmp = replaceColor(Layers[i], Color.FromArgb(0xF0, 0xF0, 0xF0), trackBarTolerance.Value);
+                        break;
+                    default:
+                        return;
+                }
+                e.Graphics.DrawImage(bmp, pictureBox1.ClientRectangle);
+            }
+        }
+        private Bitmap replaceColor(Bitmap bmp, Color targetColor, int tolerance)
+        {
+            var copy = new Bitmap(bmp);
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < copy.Height; y++)
+                {
+                    Color pixelColor = copy.GetPixel(x, y);
+                    if (localIsWithinTolerance(pixelColor, targetColor, tolerance))
+                    {
+                        copy.SetPixel(x, y, Color.Transparent);
+                    }
+                }
+            }
+            bool localIsWithinTolerance(Color pixelColor, Color targetColor, int tolerance)
+            {
+                return Math.Abs(pixelColor.R - targetColor.R) <= tolerance &&
+                        Math.Abs(pixelColor.G - targetColor.G) <= tolerance &&
+                        Math.Abs(pixelColor.B - targetColor.B) <= tolerance;
+            }
+            return copy;
+        }
+    }
+}
